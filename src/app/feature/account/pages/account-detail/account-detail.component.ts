a4dcc3b595ff47caf7d7734b00ccf15fe78d599e
Javier Vite Ramirez <javier.vite@puebla.gob.mx>
2026-03-11 17:35:32 -0600

    feat: implement monthly summary retrieval and enhance account detail display

--- src/app/feature/account/pages/account-detail/account-detail.component.ts
+++ src/app/feature/account/pages/account-detail/account-detail.component.ts
@@ -2,12 +2,11 @@ import { Component, computed, inject, input } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
-
 import { ProgressSpinner } from 'primeng/progressspinner';
-
 import { AccountService } from '../../services/account.service';
 import { CardComponent } from '@shared/components/card/card.component';
 import { AccountTransactionItemComponent } from '../../components/account-transaction-item/account-transaction-item.component';
+import {AccountSummaryCardComponent} from "../../components/account-summary-card/account-summary-card.component";
 
 @Component({
   selector: 'vrw-account-detail',
@@ -18,6 +17,7 @@ import { AccountTransactionItemComponent } from '../../components/account-transa
     RouterLink,
     ProgressSpinner,
     AccountTransactionItemComponent,
+    AccountSummaryCardComponent,
   ],
   templateUrl: './account-detail.component.html',
 })
@@ -36,19 +36,13 @@ export default class AccountDetailComponent {
     stream: ({ params }) => this.accountService.getTransactions(params.id),
   });
 
-  summaryResource = rxResource({
-    params: () => ({ id: this.id() }),
-    stream: ({ params }) => this.accountService.getMonthlySummary(params.id)
-  });
-
   account = computed(() => this.accountResource.value());
   accountStyle = computed(() => {
     const color = this.account()?.color || '#009688';
     return {
       primary: color,
-      light: `${color}1A` // 10% opacidad
+      light: `${color}1A`
     };
   });
-  currentSummary = computed(() => this.summaryResource.value()?.currentMonth ?? { income: 0, expense: 0 });
   isInitialLoading = computed(() => this.accountResource.isLoading());
 }
