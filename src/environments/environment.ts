9ba224a518204df16d81d66e592245abe7cf9e9f
Javier Vite Ramirez <50723872+Javier101098@users.noreply.github.com>
2026-09-14 19:58:20 -0600

    Update API base URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://vrwallet-api.d-teach.space/api',
+  baseUrl: 'https://vrwallet-api.javiervr.dev/api',
   push_public_key:
     'BJxs4_1APH5zgWVQ6bJ6Zyzyz7XMcQA1WLow0lEDJKItAhD2jShHOAp9jh9zIW2lFwkoJmCXHeMnmHnxydp2w24',
 };
